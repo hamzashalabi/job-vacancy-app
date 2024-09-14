4f7fb897004117219bf3ac9c432b156bf4911249
hamza shalabi <hamza.shalabi111@gmail.com>
2024-09-14 15:18:43 +0300

    added-error-messages

--- lib/api_service/api_exceptions.dart
+++ lib/api_service/api_exceptions.dart
@@ -1,10 +1,30 @@
 // response status aside from 200
-class ResponseExcpetion implements Exception {}
+class ResponseExcpetion implements Exception {
+  @override
+  String toString() {
+    return 'Server returned an invalid response. Please try again.';
+  }
+}
 
 // dio exceptions
-class DioConnectionTimeOutException implements Exception {}
+class DioConnectionTimeOutException implements Exception {
+  @override
+  String toString() {
+    return 'Connection timed out. Please check your internet connection and try again.';
+  }
+}
 
-class DioReceiveTimeOutException implements Exception {}
+class DioReceiveTimeOutException implements Exception {
+  @override
+  String toString() {
+    return 'Receive timeout. The server took too long to respond.';
+  }
+}
 
 // generic exceptions
-class GenericException implements Exception {}
+class GenericException implements Exception {
+  @override
+  String toString() {
+    return 'An unexpected error occurred. Please try again later.';
+  }
+}
